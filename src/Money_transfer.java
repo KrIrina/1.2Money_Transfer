dce546dfcf5115ce5d72ce093a816936ebe67872
Kririna <kiraonnea@gmail.com>
2021-08-09 21:51:46 +0300

    Initial commit

---
+++ src/Money_transfer.java
@@ -0,0 +1,15 @@
+public class Money_transfer {
+
+    public static void main(String[] args) {
+
+        int client_balance = 2_000_000_000;
+
+        int transfer_amount = 500_000_000;
+
+        int total = client_balance + transfer_amount;
+
+        System.out.println(total);
+
+    }
+
+}
